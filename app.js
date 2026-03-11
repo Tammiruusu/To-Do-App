252c9bb27f20a8375181c3b6459805265d05727e
Keyfr4mes <milinalii@gmail.com>
2026-03-11 12:39:45 +0200

    TODO listaq lisäys JS puolella

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
-const todoListUl = document.getElementById('todo-list');
+const todoListUL = document.getElementById('todo-list');
 
 //Luodaan TYHJÄ Array/lista jonne tallennetaan kaikki todo:t 
 let allTodos = [];
@@ -21,6 +21,40 @@ todoForm.addEventListener('submit', function(e){
 function addTodo(){
     //trim metodi poistaa turhat 'tyhjät', esim.'        jees' > 'jees'
     //todoInput.valua nappaa kiinni input kentän tekstiin
-    const todoText = todoInput.value.trim;
-    alert(todoText);
+    const todoText = todoInput.value.trim();
+    //Jos input kentässä on enemmän kirjaimia kuin 0, vasta silloin todo lisätään
+    //Ylhäällä trim poistaa välilyönnit, joten se ei lasketa 'pituuteen/length'
+
+    if(todoText.length > 0){
+    //Käytetään PUSH metodia, jotta voidaan työntää input kentän teksti arrayhin
+    allTodos.push(todoText);
+    //palauttaa päivitetyn listan, oli sinne lisätty tai poistettu asioita
+    updateTodoList();
+    //Kun todo lisätty, alla oleva komento tyhjentää kentän, tapahtuu vain jos käyttäjä
+    //lisää tekstiä, koska if lausekkeen sisällä
+    todoInput.value = "";
+    }
+}
+
+
+//päivittää listan/Arrayn kun siihen lisätään jotain tai poistetaan jotain
+function updateTodoList(){
+    //tyhjentää listan
+    todoListUL.innerHTML = "";
+    //Käytetään foreach looppia, jotta tuodaan esiin kaikki arrayn itemit näkyviin
+    allTodos.forEach((todo, todoIndex)=>{
+        //tarvitsee RETURN statementin Createtodo Itemiin, jotta tämä toimii
+        todoItem = createTodoItem(todo, todoIndex);
+        //Appendilla tuodaan todot näkyviin sivulle
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo){
+    //Luodaan uusi lista elementti, jotta voidaan lisätä todo input kentän tekstit näkyville
+    //Create elementin kautta, "li" luo lista itemin
+    const todoLI = document.createElement("li");
+    todoLI.innerHTML = todo;
+    //Palauttaa lisätyn asian Arrayhin, jolloin muutos tulee näkyviin
+    return todoLI;
 }
